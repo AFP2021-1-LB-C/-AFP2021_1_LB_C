d0140adcbaf6f3ca5b5f89275124ab0cc1795b7c
Daniel Vasas <daniel.vasas1@gmail.com>
2022-01-18 23:02:05 +0100

    Linkek megjelenítése a lista oldalon és a hiányzó címek hibájának javítása

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,12 @@
+  @if (isset($page_links))
+    @foreach ($page_links as $page_link)
+    <small class="d-block text-end mt-3">
+      <a href="{{$page_link -> link}}">{{$page_link -> label}}</a>
+    </small> 
+    @endforeach 
+  @endif
+
+
    </div>
 
 </main>
