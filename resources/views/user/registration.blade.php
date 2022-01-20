3f6e8119552d5ff68b4660b1c9103c7ef8256090
benedeklazar <82848692+benedeklazar@users.noreply.github.com>
2022-01-20 21:32:24 +0100

    [T-004] Regisztráció - Hibaüzenetek és Szerepkörök javítása

--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -26,61 +26,50 @@
                         <div class="form-group">
                             <label for="first_name"> Vezetéknév </label>
                             <input type="text" name="first_name" id="first_name" class="form-control" placeholder="Vezetéknév" value="{{ old('first_name') }}"/>
-                            {!! $errors->first('first_name', '<small class="text-danger">:message</small>') !!}
+                            {!! $errors->first('first_name', '<small class="text-danger">A vezetéknév :message</small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="last_name"> Keresztnév </label>
                             <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Keresztnév" value="{{ old('last_name') }}"/>
-                            {!! $errors->first('last_name', '<small class="text-danger">:message </small>') !!}
+                            {!! $errors->first('last_name', '<small class="text-danger">A keresztnév :message </small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="username"> Felhasználónév </label>
                             <input type="text" name="username" id="username" class="form-control" placeholder="Felhasználónév" value="{{ old('username') }}"/>
-                            {!! $errors->first('username', '<small class="text-danger">:message </small>') !!}
+                            {!! $errors->first('username', '<small class="text-danger">A felhasználónév :message </small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="email"> E-mail </label>
                             <input type="text" name="email" id="email" class="form-control" placeholder="E-mail" value="{{ old('email') }}"/>
-                            {!! $errors->first('email', '<small class="text-danger">:message</small>') !!}
+                            {!! $errors->first('email', '<small class="text-danger">Az email :message</small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="password"> Jelszó </label>
                             <input type="password" name="password" id="password" class="form-control" placeholder="Jelszó" value="{{ old('password') }}"/>
-                            {!! $errors->first('password', '<small class="text-danger">:message</small>') !!}
+                            {!! $errors->first('password', '<small class="text-danger">A jelszó :message</small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="confirm_password"> Jelszó újra </label>
                             <input type="password" name="confirm_password" id="confirm_password" class="form-control" placeholder="Jelszó Újra" value="{{ old('confirm_password') }}">
-                            {!! $errors->first('confirm_password', '<small class="text-danger">:message</small>') !!}
+                            {!! $errors->first('confirm_password', '<small class="text-danger">A jelszó újra :message</small>') !!}
                         </div>
 
                         <div class="form-group">
                             <label for="age"> Kor </label>
                             <input type="number" name="age" id="age" class="form-control" placeholder="Életkor" value="{{ old('age') }}">
-                            {!! $errors->first('age', '<small class="text-danger">:message</small>') !!}
+                            {!! $errors->first('age', '<small class="text-danger">A kor :message</small>') !!}
                         </div>
 
-                         <div class="form-group">
-                         <label for="age"> Szerepkör </label><br>
-                            <select name="role_id" id="role_id" class="form_control" value="{{ old('age') }}">
-                            @foreach ($roles as $role)             
-                            <option value="{{$role -> id}}">{{$role -> name}}</option>       
-                            @endforeach
-                            </select>
-                            
-                        </div>
-
-        
 
                     </div>
                     <div class="card-footer d-inline-block">
                         <button type="submit" class="btn btn-success"> Regisztráció </button>
-                    <p class="float-right mt-2"> Már van fiókja?  <a href="{{ url('user-login')}}" class="text-success"> Bejelentkezés </a> </p>
+                    <p class="float-right mt-2"> Már van fiókja?  <a href="{{ url('login')}}" class="text-success"> Bejelentkezés </a> </p>
                     </div>
                     @csrf
                 </div>
--- resources/views/user/user_list.blade.php
+++ resources/views/user/user_list.blade.php
@@ -1,4 +1,18 @@
+<?php use App\Models\User; ?>
+
 @include('layout.header')
+
+@if (!User::exists())
+  <table>
+<thead>
+  <thead>
+    <tr>
+      <th>Még nincs felhasználó regisztrálva!</th>
+    </tr>
+  </thead>
+  </thead>
+<tbody>
+@else
 
 <table>
 <thead>
@@ -6,16 +20,18 @@
     <th>Azonosító</th>
     <th>Név</th>
     <th>Életkor</th>
-    <th>Szerepkör Azonosító</th>
+    <th>Szerepkör</th>
     <th>Felhasználónév</th>
     <th>Email</th>
-    <th>Jelszó</th>
-    <th>Regisztrálás Dátuma</th>
-    <th>Utolsó Bejelentkezés Dátuma</th>
+    
+    <th>Regisztrálva</th>
+    <th>Utolsó Bejelentkezés</th>
 
   </tr>
 </thead>
 <tbody>
+  
+
   @foreach ($items as $item)
   <tr>
     <td>{{$item -> id}}</td>
@@ -24,7 +40,7 @@
     <td>{{$item -> role -> name}}</td>
     <td>{{$item -> username}}</td>
     <td>{{$item -> email}}</td>
-    <td>{{$item -> password}}</td>
+    
     <td>{{$item -> registration_date}}</td>
     <td>{{$item -> last_login_date}}</td>
     <td><a href="/admin/user/edit/{{$item -> id}}">Szerkesztés</a></td>
@@ -33,5 +49,7 @@
 
 </tbody>
 </table>
+
+  @endif
 
 @include('layout.footer')
