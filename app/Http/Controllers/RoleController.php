a54ed40f4b3caa5c34c6f5eba0c7d72e7881d74f
Dora Dravucz <dravucz.dora@gmail.com>
2022-01-23 23:41:09 +0100

    Felhasználói szerepkörök elérhetőségének beállítása

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,15 +14,23 @@ class RoleController extends Controller
      */
     public function index()
     {
+        $page_links = [];
+
+        if ($this->auth('role_id') !== 1) {
+            return redirect()->to('/');
+        }
+        elseif($this->auth('role_id') == 1) {
+            $page_links = array_merge($page_links, [
+                (object)['label' => 'Létrehozás', 'link' => '/admin/role/create'],
+            ]);
+        }
         $data = Role::all();
         
         return view('user.roles_list',[
             'items' => $data ,
             'page_title' => 'Szerepkörök' ,
             'page_subtitle' => 'Lista' ,
-            'page_links' => [
-                (object)['label' => 'Létrehozás', 'link' => '/admin/role/create'] ,
-            ] ,
+            'page_links' => $page_links,
         ]);
     }
 
@@ -34,6 +42,9 @@ class RoleController extends Controller
     public function create(Request $request)
     {
         //dd($request->request);  // dump and die
+        if ($this->auth('role_id') !== 1) {
+            return redirect()->to('/');
+        }
 
         $new = Role::create([
             'name' => $request->name,            
@@ -46,6 +57,10 @@ class RoleController extends Controller
 
     public function create_form()
     {
+        if ($this->auth('role_id') !== 1) {
+            return redirect()->to('/');
+        }
+
         return view('user.roles_create', [
             'page_title' => 'Szerepkörök' ,
             'page_subtitle' => 'Létrehozás' ,
@@ -82,6 +97,10 @@ class RoleController extends Controller
      */
     public function edit($id)
     {
+        if ($this->auth('role_id') !== 1) {
+            return redirect()->to('/');
+        }
+
         $data = Role::where('id', $id) -> first();
         
         return view('user.roles_edit',[
@@ -102,7 +121,10 @@ class RoleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+        if ($this->auth('role_id') !== 1) {
+            return redirect()->to('/');
+        }
+
         $new = Role::where('id', $id) -> update([
             'name' => $request->name,
         ]);
