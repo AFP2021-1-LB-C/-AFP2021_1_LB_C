228f4c12218d8be27a46373edb9ba527073e404b
Dora Dravucz <dravucz.dora@gmail.com>
2022-03-08 16:24:46 +0100

    Szinek es fontok beallitasa

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>E-learning</title>
-
-    
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka&family=Inter&family=Raleway&display=swap" rel="stylesheet">
+        <title>StudySloth</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -35,10 +36,14 @@
   <main class="container">
 
   <!-- Oldal címe -->
+<<<<<<< Updated upstream
   <div class="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
     <img class="me-3" src="/images/logo001.png" alt="" width="47" height="46">
+=======
+  <div class="d-flex align-items-center p-3 my-3 text-white bg-turquoise rounded shadow-sm">
+>>>>>>> Stashed changes
     <div class="lh-1">
-      <h1 class="h6 mb-0 text-white lh-1">{{$page_title ?? ''}}</h1>
+      <h1 class="h5 mb-0 text-white lh-1">{{$page_title ?? ''}}</h1>
       <small>{{$page_subtitle ?? ''}}</small>
     </div>
   </div>
