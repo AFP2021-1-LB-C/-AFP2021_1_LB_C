164b1a4b8cbe05e040bb742ebceb20ca278bbb4a
Daniel Vasas <daniel.vasas1@gmail.com>
2022-03-12 21:40:36 +0100

    Háttérszín váltása hozzáadása -Gomb segítségével

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -11,8 +11,21 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+
     <style>
-      .bd-placeholder-img {
+    .bg-dark .table{    
+    color: white;
+    }
+    .bg-dark .bg-body , .bg-dark thead tr th{
+    background-color: #333 !important;     
+    color: white;
+    } 
+    .bg-dark .bg-body a{
+    color: cyan !important;
+    }
+  
+
+    .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
